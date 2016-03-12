8f09f410e80c7f9ed5511df5771ca834ee76d16c
Sergiu Tripon <contact@sergiu-tripon.com>
2016-03-12 14:00:45 +0000

    Modified several files.

--- part-b/src/main.py
+++ part-b/src/main.py
@@ -26,23 +26,20 @@ def main():
     # log_bgd
     # learning_rate = 0.0001
 
-    # the four learners
-    lin_sgd_threshold = 0.1
-    # lin_bgd_threshold = 0.1
-    # log_sgd_threshold = 0.1
-    # log_bgd_threshold = 0.1
+    # threshold
+    threshold = 0.1
 
     # run linear regression with stochastic gradient descent
-    run.run(proc.lin_sgd, learning_rate, lin_sgd_threshold)
+    run.run(proc.lin_sgd, learning_rate, threshold)
 
     # run linear regression with batch gradient descent
-    # run.run(proc.lin_bgd, learning_rate, lin_bgd_threshold)
+    # run.run(proc.lin_bgd, learning_rate, threshold)
 
     # run logistic regression with stochastic gradient descent
-    # run.run(proc.log_sgd, learning_rate, log_sgd_threshold)
+    # run.run(proc.log_sgd, learning_rate, threshold)
 
     # run logistic regression with batch gradient descent
-    # run.run(proc.log_bgd, learning_rate, log_bgd_threshold)
+    # run.run(proc.log_bgd, learning_rate, threshold)
 
 
 ########################################################################################################################
